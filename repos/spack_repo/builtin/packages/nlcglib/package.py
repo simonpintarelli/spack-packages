4842c5f5cb8ff45c0e79f20258cd907c919e7f4f
Simon Pintarelli <simon.pintarelli@cscs.ch>
2026-02-20 13:42:08 +0100

    update nlcglib

--- repos/spack_repo/builtin/packages/nlcglib/package.py
+++ repos/spack_repo/builtin/packages/nlcglib/package.py
@@ -80,7 +80,7 @@ class Nlcglib(CMakePackage, CudaPackage, ROCmPackage):
         depends_on("rocsolver")
 
     with when("+cuda"):
-        depends_on("kokkos+cuda_lambda+wrapper", when="%gcc")
+        depends_on("kokkos+wrapper", when="%gcc")
         depends_on("kokkos+cuda")
         for arch in CudaPackage.cuda_arch_values:
             depends_on(f"kokkos cuda_arch={arch}", when=f"cuda_arch={arch}")
